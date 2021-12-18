6b4ca45b608fcb8eaa4616e62cdaa096cf53a4f8
Klay Clarke <klayaclarke@gmail.com>
2021-12-18 16:23:16 -0500

    reformat imports (alphabetical order)

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from datetime import date
 from functools import wraps
-from sqlalchemy import ForeignKey
-from flask_gravatar import Gravatar
-from flask_ckeditor import CKEditor
-from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-from forms import RegisterForm, LoginForm, CreatePostForm, CommentForm
-from werkzeug.security import generate_password_hash, check_password_hash
+
 from flask import Flask, render_template, redirect, url_for, flash, request, abort, Response
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from forms import RegisterForm, LoginForm, CreatePostForm, CommentForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
